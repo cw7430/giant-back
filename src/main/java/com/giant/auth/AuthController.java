36f4e64561ebb80266618b802143b9dfffd12ab0
cw7430 <cw282@naver.com>
2026-01-08 00:03:32 +0900

    로그인, 로그아웃, 토큰재발급 비지니스 로직, api 작성

---
+++ src/main/java/com/giant/auth/AuthController.java
@@ -0,0 +1,82 @@
+package com.giant.auth;
+
+import com.giant.auth.doc.SignInSuccessResponseDoc;
+import com.giant.auth.dto.request.RefreshRequestDto;
+import com.giant.auth.dto.request.SignInRequestDto;
+import com.giant.common.api.doc.error.ErrorResponseDoc;
+import com.giant.common.api.doc.success.SuccessResponseDoc;
+import com.giant.common.api.response.ResponseDto;
+import com.giant.common.api.response.SuccessResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@Tag(name = "Auth Controller", description = "계정 API")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/sign-in")
+    @Operation(summary = "로그인")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(
+                    schema = @Schema(implementation = SignInSuccessResponseDoc.class)
+            )),
+            @ApiResponse(responseCode = "500", description = "기타오류", content = @Content(
+                    schema = @Schema(implementation = ErrorResponseDoc.InternalServerError.class)
+            ))
+    })
+    public ResponseEntity<ResponseDto> signIn(
+            HttpServletResponse response,
+            @RequestBody @Valid SignInRequestDto signInRequestDto
+    ) {
+        return ResponseEntity.ok(SuccessResponseDto.ok(authService.signIn(response, signInRequestDto)));
+    }
+
+    @PostMapping("/sign-out")
+    @Operation(summary = "로그아웃")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그아웃 성공", content = @Content(
+                    schema = @Schema(implementation = SuccessResponseDoc.class)
+            )),
+            @ApiResponse(responseCode = "500", description = "기타오류", content = @Content(
+                    schema = @Schema(implementation = ErrorResponseDoc.InternalServerError.class)
+            ))
+    })
+    public ResponseEntity<ResponseDto> signOut(HttpServletResponse response) {
+        authService.signOut(response);
+        return ResponseEntity.ok(SuccessResponseDto.ok());
+    }
+
+    @PostMapping("/refresh")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "토큰 재발급 성공", content = @Content(
+                    schema = @Schema(implementation = SignInSuccessResponseDoc.class)
+            )),
+            @ApiResponse(responseCode = "500", description = "기타오류", content = @Content(
+                    schema = @Schema(implementation = ErrorResponseDoc.InternalServerError.class)
+            ))
+    })
+    @Operation(summary = "토큰 재발급")
+    public ResponseEntity<ResponseDto> refreshAccessToken(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestBody @Valid RefreshRequestDto refreshRequestDto
+    ) {
+        return ResponseEntity.ok(SuccessResponseDto.ok(authService.refreshAccessToken(request, response, refreshRequestDto)));
+    }
+}
---
+++ src/main/java/com/giant/auth/AuthService.java
@@ -0,0 +1,64 @@
+package com.giant.auth;
+
+import com.giant.auth.dto.SignInDto;
+import com.giant.auth.dto.request.RefreshRequestDto;
+import com.giant.auth.dto.request.SignInRequestDto;
+import com.giant.auth.dto.response.SignInResponseDto;
+import com.giant.auth.repository.AccountRepository;
+import com.giant.common.api.exception.CustomException;
+import com.giant.common.api.type.ResponseCode;
+import com.giant.common.config.security.JwtProvider;
+import com.giant.common.config.security.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+    private final JwtUtil jwtUtil;
+    private final AuthUtil authUtil;
+
+    public SignInResponseDto signIn(HttpServletResponse response, SignInRequestDto signInRequestDto) {
+        SignInDto signInInfo = accountRepository.findSignInInfoByUserName(signInRequestDto.userName());
+
+        if (signInInfo == null || !passwordEncoder.matches(
+                signInRequestDto.password(), signInInfo.passwordHash()
+        )
+        ) throw new CustomException(ResponseCode.LOGIN_ERROR);
+
+        authUtil.validateAccountRole(signInInfo);
+
+        String accessToken = authUtil.generateAndSetTokens(response, signInInfo, signInRequestDto.isAuto());
+
+        return authUtil.createSignInResponse(signInInfo, accessToken);
+    }
+
+    public void signOut(HttpServletResponse response) {
+        jwtUtil.removeRefreshTokenFromCookie(response);
+    }
+
+    public SignInResponseDto refreshAccessToken(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            RefreshRequestDto refreshRequestDto
+    ) {
+        Long accountId = jwtUtil.extractUserIdFromRefreshToken(request);
+        SignInDto refreshInfo = accountRepository.findRefreshInfoByAccountId(accountId);
+
+        if (refreshInfo == null) throw new CustomException(ResponseCode.LOGIN_ERROR);
+        authUtil.validateAccountRole(refreshInfo);
+
+        String accessToken = jwtProvider.generateAccessToken(refreshInfo.accountId(), refreshInfo.accountRoleId());
+        if (refreshRequestDto.isAuto()) {
+            jwtUtil.addRefreshTokenToCookie(response, jwtProvider.generateRefreshToken(accountId, true), true);
+        }
+
+        return authUtil.createSignInResponse(refreshInfo, accessToken);
+    }
+}
