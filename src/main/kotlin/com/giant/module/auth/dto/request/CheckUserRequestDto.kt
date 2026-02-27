9d4632784b97ec5fa2808dbe5f82d87d20a76cd5
cw7430 <cw282@naver.com>
2026-02-27 18:34:46 +0900

    아이디 유효성 검사 추가

--- src/main/kotlin/com/giant/module/auth/dto/request/CheckUserRequestDto.kt
+++ src/main/kotlin/com/giant/module/auth/dto/request/CheckUserRequestDto.kt
@@ -1,8 +1,13 @@
 package com.giant.module.auth.dto.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 
 data class CheckUserRequestDto (
     @NotBlank(message = "아이디를 입력해주세요.")
+    @Pattern(
+        regexp = "^(?=.*[a-zA-Z])[a-zA-Z0-9]{5,25}$",
+        message = "아이디는 5자 이상 25자 이하, 영문 또는 영문, 숫자의 조합이어야 합니다."
+    )
     var userName: String
 )
